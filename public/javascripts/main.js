e6f1934267200a5f86881bc18013886991862b2c
Michael Sprague <mikesprague@gmail.com>
2019-02-10 17:35:17 -0500

    :construction: Add misc WIP
    Adds form module, refactors google js out of layout and into notifications page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,74 +1,34 @@
 import '../sass/style.scss';
-import { $, $$ } from './modules/bling';
-import autocomplete from './modules/autocomplete';
-import * as image from './modules/image';
+import { $ } from './modules/bling';
+import initAutocomplete from './modules/autocomplete';
+import {
+  focusFormField,
+  initFoodPrefCheckboxes,
+  initNotificationPhotoField,
+} from './modules/form';
 
-const initFoodPrefCheckboxes = ((checkboxSelector) => {
-  const firstCheckBox = $(`input[name="${checkboxSelector}"]`);
-  if (firstCheckBox) {
-    firstCheckBox.on('change', function () {
-      if (this.checked) {
-        $$(`input[name="${checkboxSelector}"]`).map((checkbox) => {
-          /* eslint-disable no-param-reassign */
-          checkbox.checked = false;
-          /* eslint-enable no-param-reassign */
-          return checkbox;
-        });
-        this.checked = true;
-      }
-    });
-    $$(`input[name="${checkboxSelector}"]`).on('change', function () {
-      if (this.value !== firstCheckBox.value) {
-        firstCheckBox.checked = false;
-      }
-    });
-  }
-});
+// use try catches until I decide to split up files or conditionally load in one
 
-const initAllCheckboxes = () => {
-  // hacky solution (should replace)
-  // allows for one script one all pages
-  try {
-    initFoodPrefCheckboxes('foodExclusions');
-  } catch (e) {
-    // ignore
-  }
-};
+try {
+  initFoodPrefCheckboxes('foodExclusions');
+} catch (e) {
+  // ignore
+}
 
-const initAutoComplete = () => {
-  // hacky solution (should replace)
-  // allows for one script one all pages
-  try {
-    autocomplete($('#address'), $('#lat'), $('#lng'));
-  } catch (e) {
-    // ignore
-  }
-};
+try {
+  initAutocomplete($('#address'), $('#lat'), $('#lng'));
+} catch (e) {
+  // ignore
+}
 
-const initFocusEmail = () => {
-  // hacky solution (should replace)
-  // allows for one script one all pages
-  try {
-    $('#emailLogin').focus();
-  } catch (e) {
-    // ignore
-  }
-};
+try {
+  initNotificationPhotoField('#photo', '.photo-preview');
+} catch (e) {
+  // ignore
+}
 
-const initNotificationPhoto = () => {
-  try {
-    const notificationPhoto = document.querySelector('#photo');
-    notificationPhoto.on('change', async function () {
-      const imageData = await image.getImageFromFileInput(this);
-      // console.log(imageData);
-      image.setPreviewImage('.photo-preview', imageData[0]);
-    });
-  } catch (e) {
-    // ignore
-  }
-};
-
-initAllCheckboxes();
-initAutoComplete();
-initFocusEmail();
-initNotificationPhoto();
+try {
+  focusFormField('#emailLogin');
+} catch (e) {
+  // ignore
+}
---
+++ public/javascripts/modules/form.js
@@ -0,0 +1,37 @@
+import { $, $$ } from './bling';
+import { getImageFromFileInput, setPreviewImage } from './image';
+
+export function initFoodPrefCheckboxes (checkboxSelector) {
+  const firstCheckBox = $(`input[name="${checkboxSelector}"]`);
+  if (firstCheckBox) {
+    firstCheckBox.on('change', function () {
+      if (this.checked) {
+        $$(`input[name="${checkboxSelector}"]`).map((checkbox) => {
+          /* eslint-disable no-param-reassign */
+          checkbox.checked = false;
+          /* eslint-enable no-param-reassign */
+          return checkbox;
+        });
+        this.checked = true;
+      }
+    });
+    $$(`input[name="${checkboxSelector}"]`).on('change', function () {
+      if (this.value !== firstCheckBox.value) {
+        firstCheckBox.checked = false;
+      }
+    });
+  }
+}
+
+export function focusFormField(formElementSelector) {
+  $(formElementSelector).focus();
+}
+
+export function initNotificationPhotoField(fileElementSelector, imgToShowPreviewSelector) {
+  const photoFileElement = $(fileElementSelector);
+  photoFileElement.on('change', async function () {
+    const imageData = await getImageFromFileInput(this);
+    // console.log(imageData);
+    setPreviewImage(imgToShowPreviewSelector, imageData[0]);
+  });
+}
